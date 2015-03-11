bb9aa7a7805f91f4d4138307f467701517b2cfd0
Chaos <haos.engine@gmail.com>
2015-03-11 23:56:50 +0100

    cleaning and testing of emerge

--- amd64/container-build.sh
+++ amd64/container-build.sh
@@ -21,27 +21,29 @@ chmod +x /busybox
 #/busybox cp /container/portage-latest.tar.xz .
 /busybox xzcat portage-latest.tar.xz | tar -C /usr/ -xvf -
 cd /
-#commit suecide
+#commit suicide
 /busybox rm -rf newWorldOrder /busybox /container-build.sh /portage-latest.tar.xz
-#cp -rafld lib64 lib lib32 bin /
-
 #busybox chroot ./
-## Setup the rc_sys
-#sed -e 's/#rc_sys=""/rc_sys="lxc"/g' -i /etc/rc.conf
 
-## Setup the net.lo runlevel
-#ln -s /etc/init.d/net.lo /run/openrc/started/net.lo
 
-## Setup the net.eth0 runlevel
-#ln -s /etc/init.d/net.lo /etc/init.d/net.eth0
-#ln -s /etc/init.d/net.eth0 /run/openrc/started/net.eth0
 
-## By default, UTC system
-#echo 'UTC' > /etc/timezone
 
-## Self destruct
-#rm /Dockerfile
-#rm /build.sh
+# Setup the rc_sys
+sed -e 's/#rc_sys=""/rc_sys="lxc"/g' -i /etc/rc.conf
 
-#echo "Bootstrapped ${stage3} into /:"
-#ls --color -lah
+# Setup the net.lo runlevel
+ln -s /etc/init.d/net.lo /run/openrc/started/net.lo
+
+# Setup the net.eth0 runlevel
+ln -s /etc/init.d/net.lo /etc/init.d/net.eth0
+ln -s /etc/init.d/net.eth0 /run/openrc/started/net.eth0
+
+# By default, UTC system
+echo 'UTC' > /etc/timezone
+
+# Self destruct
+rm /Dockerfile
+rm /build.sh
+
+echo "Bootstrapped ${stage3} into /:"
+ls --color -lah
