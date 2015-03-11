14bb3869f0d905b87a0027c6eab12b3b957b03aa
Chaos <haos.engine@gmail.com>
2015-03-11 21:19:45 +0100

    reverted original build.sh

--- amd64/build.sh
+++ amd64/build.sh
@@ -1,40 +1,26 @@
-stage3_suffix="" # e.g. -hardened
-dist="http://distfiles.gentoo.org/releases/amd64/autobuilds"
-stage3="$(wget -O- ${dist}/latest-stage3-amd64${suffix}.txt | tail -n 1 | cut -f 1 -d ' ')"
+#!/bin/bash
 
-mkdir newWorldOrder; cd newWorldOrder
-echo "Downloading and extracting ${stage3}..."
-#wget -c "${dist}/${stage3}"
-cp /container/amd64/stage3-amd64-20150305.tar.bz2 .
-bunzip2 -c $(basename ${stage3}) | tar --exclude "./etc/hosts" --exclude "./sys/*" -xvf -
-#wget http://www.busybox.net/downloads/binaries/latest/busybox-x86_64
-cp /container/busybox-x86_64 /busybox
-chmod +x /busybox
-/busybox rm -rf /lib64 /lib /lib32 /usr /var /bin /opt /mnt /media /root
-/busybox mv -f lib64 /
-/busybox mv -f lib32 /
-/busybox mv -f lib /
-/busybox mv -f * /
-#rm -rf /lib /lib64 /usr/bin /bin
-#cp -rafld lib64 lib lib32 bin /
+die(){ echo "$@" 1>&2; exit 1; }
 
-#busybox chroot ./
-## Setup the rc_sys
-#sed -e 's/#rc_sys=""/rc_sys="lxc"/g' -i /etc/rc.conf
+base_url="http://distfiles.gentoo.org/releases/amd64/autobuilds"
 
-## Setup the net.lo runlevel
-#ln -s /etc/init.d/net.lo /run/openrc/started/net.lo
+latest_stage3=$(curl "${base_url}/latest-stage3-amd64.txt" 2>/dev/null | grep -v '#' | tail -n1 | cut -f 1 -d ' ')
+stage3=$(basename "${latest_stage3}")
 
-## Setup the net.eth0 runlevel
-#ln -s /etc/init.d/net.lo /etc/init.d/net.eth0
-#ln -s /etc/init.d/net.eth0 /run/openrc/started/net.eth0
+[ ! -f "${stage3}" ] && xz=true || xz=false
 
-## By default, UTC system
-#echo 'UTC' > /etc/timezone
+wget -nc "${base_url}/${latest_stage3}" || die "Could not download stage3"
+wget -nc "${base_url}/${latest_stage3}.DIGESTS.asc" || die "Could not download digests"
+wget -nc "${base_url}/${latest_stage3}.CONTENTS" || die "Could not download contents"
+sha512_digests=$(grep -A1 SHA512 "${stage3}.DIGESTS.asc" | grep -v '^--')
+gpg --verify "${stage3}.DIGESTS.asc" || die "Insecure digests"
+echo "${sha512_digests}" | sha512sum -c || die "Checksum validation failed"
 
-## Self destruct
-#rm /Dockerfile
-#rm /build.sh
+if [ ${xz} == true ] || [ ! -f stage3-amd64.tar.xz ]; then
+	echo "Transforming bz2 tarball to xz (golang bug). This will take some time..."
+	bunzip2 -c "${stage3}" | xz -z > stage3-amd64.tar.xz || die "Failed to recompress to xz"
+fi
+echo "I'm done with the stage3."
 
-#echo "Bootstrapped ${stage3} into /:"
-#ls --color -lah
+echo "Building docker Gentoo image now..."
+docker build -t gentoo .
