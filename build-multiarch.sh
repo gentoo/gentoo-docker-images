6219ee847cf38d60753bde2bbe91b0130279c840
Aaron Simmons <paleozogt@gmail.com>
2020-02-17 14:27:24 -0800

    using docker-copyedit to modify the arch
    Signed-off-by: Alexys Jacob <ultrabug@gentoo.org>

---
+++ build-multiarch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker manifest create gentoo/stage3 \
+	gentoo/stage3-amd64  \
+	gentoo/stage3-x86    \
+	gentoo/stage3-armv7a \
+	gentoo/stage3-amd64  \
+	gentoo/stage3-ppc    \
+	gentoo/stage3-ppc64  \
+	gentoo/stage3-ppc64le
--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ fi
 
 # Split the TARGET variable into three elements separated by hyphens
 IFS=- read -r NAME ARCH SUFFIX <<< "${TARGET}"
+DOCKER_ARCH="${ARCH}"
 
 # Ensure upstream directories for stage3-amd64-hardened+nomultilib work
 SUFFIX=${SUFFIX/-/+}
@@ -21,12 +22,14 @@ ORG=${ORG:-gentoo}
 
 # x86 requires the i686 subfolder
 if [[ "${ARCH}" == "x86" ]]; then
+	DOCKER_ARCH="386"
 	MICROARCH="i686"
 	BOOTSTRAP="multiarch/alpine:x86-v3.7"
 elif [[ "${ARCH}" = ppc* ]]; then
 	MICROARCH="${ARCH}"
 	ARCH=ppc
 elif [[ "${ARCH}" = arm* ]]; then
+	DOCKER_ARCH=$(echo $ARCH | sed -e 's-\(v.\).*-/\1-g')
 	MICROARCH="${ARCH}"
 	ARCH=arm
 else
@@ -40,4 +43,6 @@ fi
 
 set -x
 docker build --build-arg ARCH="${ARCH}" --build-arg MICROARCH="${MICROARCH}" --build-arg BOOTSTRAP="${BOOTSTRAP}" --build-arg SUFFIX="${SUFFIX}"  -t "${ORG}/${TARGET}:${VERSION}" -f "${NAME}.Dockerfile" .
+docker-copyedit/docker-copyedit.py FROM "${ORG}/${TARGET}:${VERSION}" INTO "${ORG}/${TARGET}:${VERSION}" -vv \
+    set arch ${DOCKER_ARCH}
 docker tag "${ORG}/${TARGET}:${VERSION}" "${ORG}/${TARGET}:latest"
