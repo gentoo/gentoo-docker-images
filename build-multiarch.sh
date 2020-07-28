1d08b3e4e41a6cd7a843be7ffd9a32d6924f880c
Michael Everitt <gentoo@veremit.xyz>
2020-07-28 15:57:59 +0100

    Dump armv7a builds temporarily whilst we triage them
    Signed-off-by: Michael Everitt <gentoo@veremit.xyz>

--- build-multiarch.sh
+++ build-multiarch.sh
@@ -2,7 +2,6 @@
 docker manifest create gentoo/stage3 \
 	gentoo/stage3-amd64  \
 	gentoo/stage3-x86    \
-	gentoo/stage3-armv7a \
 	gentoo/stage3-amd64  \
 	gentoo/stage3-ppc    \
 	gentoo/stage3-ppc64  \
