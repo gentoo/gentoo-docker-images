gentoo/gentoo-docker-images

commit 046cfa43e2470ba3cd37e0764a7800024193e51a

python.Dockerfile: pull from stable ::gentoo branch, not master
We don't want a repo w/ no metadata or CI running on it, especially if
upstreams may be using this in their own CI or something.

Signed-off-by: Sam James <sam@gentoo.org>

python.Dockerfile
@@ -16,7 +16,7 @@ RUN <<-EOF
     echo 'dev-vcs/git -perl' >> /etc/portage/package.use/git.conf
 
     # install ::gentoo
-    wget --progress=dot:mega -O - https://github.com/gentoo-mirror/gentoo/archive/master.tar.gz | tar -xz
+    wget --progress=dot:mega -O - https://github.com/gentoo-mirror/gentoo/archive/refs/heads/stable.tar.gz | tar -xz
     mv gentoo-master /var/db/repos/gentoo
 
     # main job

commit 7d19156e5a49b7c0b6593953387797069b614c10

python.Dockerfile: fix mv command
Fixes: 046cfa43e2470ba3cd37e0764a7800024193e51a
Signed-off-by: Sam James <sam@gentoo.org>

python.Dockerfile
@@ -17,7 +17,7 @@ RUN <<-EOF
 
     # install ::gentoo
     wget --progress=dot:mega -O - https://github.com/gentoo-mirror/gentoo/archive/refs/heads/stable.tar.gz | tar -xz
-    mv gentoo-master /var/db/repos/gentoo
+    mv gentoo-stable /var/db/repos/gentoo
 
     # main job
     emerge -1vnt --jobs dev-python/tox app-arch/lzip dev-lang/rust-bin dev-vcs/git \