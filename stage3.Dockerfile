gentoo/gentoo-docker-images

commit 69b54f245882605ddc46e03fb066f885352deb07

stage3.Dockerfile: use `--output` with cleartext signatures
The latest*.txt files use cleartext signatures, so use `gpg --output ...`
and operate on that instead. This means we're definitely operating on
the verified content and nothing else.

See https://gnupg.org/blog/20251226-cleartext-signatures.html.

Signed-off-by: Sam James <sam@gentoo.org>

stage3.Dockerfile
@@ -43,8 +43,8 @@ RUN <<-EOF
 
     # obtain and extract stage3
     wget -q -- "${DIST}/latest-stage3-${MICROARCH}${SUFFIX}.txt"
-    gpg --batch --verify -- "latest-stage3-${MICROARCH}${SUFFIX}.txt"
-    STAGE3PATH="$(sed -n '6p' "latest-stage3-${MICROARCH}${SUFFIX}.txt" | cut -f 1 -d ' ')"
+    gpg --batch --output latest.txt --verify -- "latest-stage3-${MICROARCH}${SUFFIX}.txt"
+    STAGE3PATH="$(sed -n '6p' "latest.txt" | cut -f 1 -d ' ')"
     echo "STAGE3PATH:" ${STAGE3PATH}
     STAGE3="$(basename ${STAGE3PATH})"
     wget -q "${DIST}/${STAGE3PATH}" "${DIST}/${STAGE3PATH}.asc"

commit e5dd1c48cf7cc165f7e6790b6fa2a8c8ac580c7e

stage3.Dockerfile: fix sed for --output change
Fixes: 69b54f245882605ddc46e03fb066f885352deb07
Signed-off-by: Sam James <sam@gentoo.org>

stage3.Dockerfile
@@ -44,7 +44,7 @@ RUN <<-EOF
     # obtain and extract stage3
     wget -q -- "${DIST}/latest-stage3-${MICROARCH}${SUFFIX}.txt"
     gpg --batch --output latest.txt --verify -- "latest-stage3-${MICROARCH}${SUFFIX}.txt"
-    STAGE3PATH="$(sed -n '6p' "latest.txt" | cut -f 1 -d ' ')"
+    STAGE3PATH="$(sed -n '3p' "latest.txt" | cut -f 1 -d ' ')"
     echo "STAGE3PATH:" ${STAGE3PATH}
     STAGE3="$(basename ${STAGE3PATH})"
     wget -q "${DIST}/${STAGE3PATH}" "${DIST}/${STAGE3PATH}.asc"